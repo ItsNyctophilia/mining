0098faa25572b65d2a312a9ace61ec60b96f05df
Raymone Miller <miller_raymone@hotmail.com>
2023-02-27 14:24:22 -0500

    Refactor test: test_drone_init

--- test/test_drone.py
+++ test/test_drone.py
@@ -7,9 +7,12 @@ from zerg.drones.drone import Drone
 class TestDrone(unittest.TestCase):
     """Test class for drone zerg units."""
 
-    def test_drone_init(self):
+    def setUp(self) -> None:
         health = 1
         capacity = 5
         moves = 1
-        drone = Drone(health, capacity, moves)
-        self.assertIsInstance(drone, Drone)
+        self.drone_ = Drone(health, capacity, moves)
+        return
+
+    def test_drone_init(self):
+        self.assertIsInstance(self.drone_, Drone)

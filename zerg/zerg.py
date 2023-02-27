3bb37ca9b16fc450d27fe366a53cd53673fa8bd2
Raymone Miller <miller_raymone@hotmail.com>
2023-02-27 16:07:43 -0500

    Passed test: test_drones_action

--- zerg/zerg.py
+++ zerg/zerg.py
@@ -2,6 +2,7 @@
 
 
 from abc import ABC
+from enum import Enum
 
 from utils.context import Context
 
@@ -9,8 +10,14 @@ from utils.context import Context
 class Zerg(ABC):
     """Abstract base class for all zerg units"""
 
+    class Directions(Enum):
+        NORTH = "NORTH"
+        SOUTH = "SOUTH"
+        EAST = "EAST"
+        WEST = "WEST"
+
     def __init__(self, health: int) -> None:
         self._health = health
 
     def action(self, context: Context) -> str:
-        return ""
+        return Zerg.Directions.NORTH.value

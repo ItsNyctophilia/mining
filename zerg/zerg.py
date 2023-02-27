fcd34ba2c27b8d8364428a74adaf2c284912078b
Raymone Miller <miller_raymone@hotmail.com>
2023-02-27 16:00:25 -0500

    Add action() method in zerg class

--- zerg/zerg.py
+++ zerg/zerg.py
@@ -3,9 +3,14 @@
 
 from abc import ABC
 
+from utils.context import Context
+
 
 class Zerg(ABC):
     """Abstract base class for all zerg units"""
 
     def __init__(self, health: int) -> None:
         self._health = health
+
+    def action(self, context: Context) -> str:
+        return ""

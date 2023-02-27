335023a3aa648b0f653fb46b0a77b0b053979f8e
Raymone Miller <miller_raymone@hotmail.com>
2023-02-27 14:29:27 -0500

    Failed test: test_scout_init

--- test/test_drone.py
+++ test/test_drone.py
@@ -12,7 +12,13 @@ class TestDrone(unittest.TestCase):
         capacity = 5
         moves = 1
         self.drone_ = Drone(health, capacity, moves)
-        return
 
     def test_drone_init(self):
         self.assertIsInstance(self.drone_, Drone)
+
+    def test_scout_init(self):
+        health = 1
+        capacity = 5
+        moves = 1
+        self.drone_ = ScoutDrone(health, capacity, moves)
+        self.assertIsInstance(self.drone_, ScoutDrone)

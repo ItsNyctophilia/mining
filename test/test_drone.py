da960b31a8377d63ac8ec27e5e381559f7862b4f
Raymone Miller <miller_raymone@hotmail.com>
2023-02-27 21:28:12 -0500

    Use named tuples for custom drone stats in tests

--- test/test_drone.py
+++ test/test_drone.py
@@ -1,7 +1,7 @@
 """Test class for drone zerg units."""
 import random
 import unittest
-from typing import List, Tuple, Type
+from typing import List, NamedTuple, Type
 
 from utils.context import Context
 from zerg.drones.drone import Drone
@@ -12,14 +12,15 @@ from zerg.drones.scout import ScoutDrone
 class TestDrone(unittest.TestCase):
     """Test class for drone zerg units."""
 
-    # index for health in custom_drons_stats
-    HEALTH = 0
-    # index for capacity in custom_drons_stats
-    CAPACITY = 1
-    # index for moves in custom_drons_stats
-    MOVES = 2
-    # index for initializer in custom_drons_stats
-    INIT = 3
+    CustomDroneStat = NamedTuple(
+        "CustomDroneStat",
+        [
+            ("health", int),
+            ("capacity", int),
+            ("moves", int),
+            ("init", Type[Drone]),
+        ],
+    )
 
     def setUp(self) -> None:
         self.phony_context_ = Context()
@@ -28,14 +29,14 @@ class TestDrone(unittest.TestCase):
         self.base_miner_ = MinerDrone()
         self.custom_drone_count_ = 50
         self.custom_drones_: List[Drone] = []
-        self.custom_drone_stats_: List[Tuple[int, int, int, Type[Drone]]] = []
+        self.custom_drone_stats_: List[TestDrone.CustomDroneStat] = []
         for _ in range(self.custom_drone_count_):
             health = random.randrange(10, 101, 10)
             capacity = random.randrange(5, 51, 5)
             moves = random.randrange(1, 11, 5)
             Blueprint = Drone.drone_blueprint(health, capacity, moves)
             self.custom_drone_stats_.append(
-                (health, capacity, moves, Blueprint)
+                self.CustomDroneStat(health, capacity, moves, Blueprint)
             )
             self.custom_drones_.append(Blueprint())
 
@@ -60,17 +61,15 @@ class TestDrone(unittest.TestCase):
 
     def test_dynamic_drone(self):
         for drone_n in range(self.custom_drone_count_):
-            self.assertIsInstance(
-                self.custom_drone_stats_[drone_n][self.INIT], type
-            )
+            self.assertIsInstance(self.custom_drone_stats_[drone_n].init, type)
             self.assertIsInstance(self.custom_drones_[drone_n], Drone)
 
     def test_drone_cost(self):
         for drone_n in range(self.custom_drone_count_):
             check_cost = self.custom_drones_[drone_n].get_init_cost()
-            health = self.custom_drone_stats_[drone_n][self.HEALTH]
-            capacity = self.custom_drone_stats_[drone_n][self.CAPACITY]
-            moves = self.custom_drone_stats_[drone_n][self.MOVES]
+            health = self.custom_drone_stats_[drone_n].health
+            capacity = self.custom_drone_stats_[drone_n].capacity
+            moves = self.custom_drone_stats_[drone_n].moves
             total_cost = (health / 10) + (capacity / 5) + (moves * 3)
             self.assertEqual(check_cost, total_cost)
 

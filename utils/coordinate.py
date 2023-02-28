d1476707057d7ae9f660221c4840236d11ddf52a
Raymone Miller <miller_raymone@hotmail.com>
2023-02-28 10:00:05 -0500

    Add difference method

--- utils/coordinate.py
+++ utils/coordinate.py
@@ -1,7 +1,12 @@
 """(X, Y) coordinates for a grid system."""
+from __future__ import annotations
+
 from typing import NamedTuple
 
 
 class Coordinate(NamedTuple):
     x: int
     y: int
+
+    def difference(self, other_coord: Coordinate):
+        return (other_coord.x - self.x, other_coord.y - self.y)

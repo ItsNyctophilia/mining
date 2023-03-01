38e5bb92b80a270ba007906139a17a9e6566285a
Josh <jcarroll0310@gmail.com>
2023-03-01 08:35:53 -0500

    Add drawing of unicode characters

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from dashboard import Dashboard
 from map import *
 from tkinter import *
 root = Tk()
-example = Dashboard(root)
+example = Map(root)
+example.log.insert('1.0', "Cocopebbles\n")
+example.log.insert('2.0', "Cocopebbles\n")
+example.log.insert('3.0', "Cocopebbles\n")
+example.log.insert('3.5', "Cocopebbles\n")
+example.write_up('unknown', 1, 2)
 
 mainloop()
--- map.py
+++ map.py
@@ -9,6 +9,20 @@ class Map(tkinter.Toplevel):
         super().__init__(parent)
         self.geometry("300x200+300+0")
         self.title("Map")
-        self.log = tkinter.Text(self, height=50,  width=50)
+        self.log = tkinter.Text(self, width=30,  height=30)
         self.log.pack()
+
+
+    def write_up(self, string, x, y):
+        unicode_dict = {
+            'wall' : u'\u2589',
+            'acid' : u'\u2600',
+            'minerals' : u'\u2662',
+            'drone' : u'\u26DF',
+            'deployment' : u'\u25BD',
+            'unknown' : u'\u26F6'
+        }
+        unicode_character = unicode_dict[string]
+        coordinates = f'{y}.{x}'
+        self.log.insert(coordinates, unicode_character)
         

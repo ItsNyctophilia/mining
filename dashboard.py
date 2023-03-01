74cab2fc9734d40d264121f27b7a3087548dfb5c
Josh <jcarroll0310@gmail.com>
2023-03-01 17:48:47 -0500

    add table for drones

--- dashboard.py
+++ dashboard.py
@@ -23,18 +23,20 @@ class Dashboard(tkinter.Toplevel):
         super().__init__(parent)
         self.photo = tkinter.PhotoImage(file='icon.png')
 
+        self.configure(bg='#2C292C')
+        # Configure the style of Heading in Treeview widget
         self.wm_iconphoto(False, self.photo)
         self.title("Overlord's Dashboard")
-        self.map_tree = self.make_tree("top", "Map", "Map ID")
-        self.map_tree.grid(row=0, column=0)
-        self.turn_tree = self.make_tree("top", "Tick", "Action")
-        self.turn_tree.grid(row=0, column=1)
-        self.turn_tree1 = self.make_fat_tree("top", "Tick", "Action")
-        self.turn_tree1.grid(row=1,column = 0, columnspan = 2)
+        self.map_tree = self.make_tree("Map", "Map ID")
+        self.map_tree.grid(row=0, column=0,padx=(20, 20), pady=(20, 20))
+        self.turn_tree = self.make_tree("Tick", "Action")
+        self.turn_tree.grid(row=0, column=1,padx=(20, 20), pady=(20, 20))
+        self.drone_tree = self.make_drone_tree()
+        self.drone_tree.grid(row=1,column = 0, columnspan = 2,padx=(20, 20), pady=(20, 20))
         # self.map_tree.insert('', 'end', text='Listbox', values=('15KB', 'Yesterday'))
 
     # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
-    def make_tree(self, aligment, column1, column2):
+    def make_tree(self, column1, column2):
         """
         Builds trees for the dasboard to use, dashboards typically serve spreadhsheets in the gui.
         """
@@ -43,7 +45,7 @@ class Dashboard(tkinter.Toplevel):
         s.theme_use('clam')
 
         # Configure the style of Heading in Treeview widget
-        s.configure('Treeview.Heading', background="green3")
+        s.configure('Treeview.Heading', background="#ad73ac")
 
 
         # Using treeview widget
@@ -68,7 +70,7 @@ class Dashboard(tkinter.Toplevel):
     # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
 
         # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
-    def make_fat_tree(self, aligment, column1, column2):
+    def make_drone_tree(self):
         """
         Builds trees for the dasboard to use, dashboards typically serve spreadhsheets in the gui.
         """
@@ -77,7 +79,7 @@ class Dashboard(tkinter.Toplevel):
         s.theme_use('clam')
 
         # Configure the style of Heading in Treeview widget
-        s.configure('Treeview.Heading', background="green3")
+        s.configure('Treeview.Heading', background="#ad73ac")
 
 
         # Using treeview widget
@@ -98,9 +100,9 @@ class Dashboard(tkinter.Toplevel):
 
         # Assigning the heading names to the
         # respective columns
-        treev.heading("1", text=column1)
-        treev.heading("2", text=column2)
-        treev.heading("3", text="a")
-        treev.heading("4", text="b")
+        treev.heading("1", text="Drone")
+        treev.heading("2", text="Health")
+        treev.heading("3", text="Capacity")
+        treev.heading("4", text="Moves")
         return treev
     # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/

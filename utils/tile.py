a4132119dcb4c4ff95782612c90a743f17fe0277
Raymone Miller <miller_raymone@hotmail.com>
2023-03-01 01:09:34 -0500

    Passed test: test_tile_immutable_coordinate

--- utils/tile.py
+++ utils/tile.py
@@ -1,7 +1,7 @@
 """A single tile on the map."""
 
 
-from dataclasses import dataclass
+from dataclasses import InitVar, dataclass
 from typing import Optional
 
 from utils import Coordinate, Icon
@@ -9,6 +9,14 @@ from utils import Coordinate, Icon
 
 @dataclass
 class Tile:
-    coordinate: Coordinate
+    location: InitVar[Coordinate]
     icon: Optional[Icon] = None
     discovered: bool = False
+
+    def __post_init__(self, location):
+        self._coordinate = location
+
+    @property
+    def coordinate(self) -> Coordinate:
+        """The coordinates of this tile on the map."""
+        return self._coordinate

150bcd802b3c67f028993ad6bcb32b0fa37ffc63
Josh <jcarroll0310@gmail.com>
2023-03-01 16:11:38 -0500

    add a third table

--- dashboard.py
+++ dashboard.py
@@ -21,12 +21,16 @@ class Dashboard(tkinter.Toplevel):
         serves as the constructore for a dashboard object
         """
         super().__init__(parent)
-        photo = tkinter.PhotoImage(file='icon.png')
-        self.wm_iconphoto(False, photo)
-        self.minsize(500, 300)
+        self.photo = tkinter.PhotoImage(file='icon.png')
+
+        self.wm_iconphoto(False, self.photo)
         self.title("Overlord's Dashboard")
         self.map_tree = self.make_tree("top", "Map", "Map ID")
-        self.turn_tree = self.make_tree("bottom", "Tick", "Action")
+        self.map_tree.grid(row=0, column=0)
+        self.turn_tree = self.make_tree("top", "Tick", "Action")
+        self.turn_tree.grid(row=0, column=1)
+        self.turn_tree1 = self.make_fat_tree("top", "Tick", "Action")
+        self.turn_tree1.grid(row=1,column = 0, columnspan = 2)
         # self.map_tree.insert('', 'end', text='Listbox', values=('15KB', 'Yesterday'))
 
     # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
@@ -34,11 +38,16 @@ class Dashboard(tkinter.Toplevel):
         """
         Builds trees for the dasboard to use, dashboards typically serve spreadhsheets in the gui.
         """
+
+        s = ttk.Style()
+        s.theme_use('clam')
+
+        # Configure the style of Heading in Treeview widget
+        s.configure('Treeview.Heading', background="green3")
+
+
         # Using treeview widget
         treev = ttk.Treeview(self, selectmode='browse')
-
-        # Calling pack method w.r.to treeview
-        treev.pack(side=aligment)
 
         # Defining number of columns
         treev["columns"] = ("1", "2")
@@ -55,5 +64,43 @@ class Dashboard(tkinter.Toplevel):
         # respective columns
         treev.heading("1", text=column1)
         treev.heading("2", text=column2)
+        return treev
+    # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
+
+        # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
+    def make_fat_tree(self, aligment, column1, column2):
+        """
+        Builds trees for the dasboard to use, dashboards typically serve spreadhsheets in the gui.
+        """
+
+        s = ttk.Style()
+        s.theme_use('clam')
+
+        # Configure the style of Heading in Treeview widget
+        s.configure('Treeview.Heading', background="green3")
+
+
+        # Using treeview widget
+        treev = ttk.Treeview(self, selectmode='browse')
+
+        # Defining number of columns
+        treev["columns"] = ("1", "2", "3", "4")
+
+        # Defining heading
+        treev['show'] = 'headings'
+
+        # Assigning the width and anchor to  the
+        # respective columns
+        treev.column("1", width=180, anchor='c')
+        treev.column("2", width=180, anchor='se')
+        treev.column("3", width=180, anchor='se')
+        treev.column("4", width=180, anchor='se')
+
+        # Assigning the heading names to the
+        # respective columns
+        treev.heading("1", text=column1)
+        treev.heading("2", text=column2)
+        treev.heading("3", text="a")
+        treev.heading("4", text="b")
         return treev
     # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from dashboard import Dashboard
 from map import *
 from tkinter import *
 root = Tk()
-example = Map(root)
-example.log.insert('1.0', "Cocopebbles\n")
-example.log.insert('2.0', "Cocopebbles\n")
-example.log.insert('3.0', "Cocopebbles\n")
-example.log.insert('3.5', "Cocopebbles\n")
-example.write_up('unknown', 1, 2)
+example = Dashboard(root)
 
 mainloop()

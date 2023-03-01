dde9b86d6d29225b5f0d07640d224cc9f0065dae
Josh <jcarroll0310@gmail.com>
2023-03-01 09:12:15 -0500

    Add doc strings

--- dashboard.py
+++ dashboard.py
@@ -1,43 +1,59 @@
-#!/usr/local/bin/python
+"""
+This serves to define the dashboard clas, defines the attributes it has along with 
+the methods that it uses.
+
+"""
+
+# !/usr/local/bin/python
 
 import random
-import tkinter 
+import tkinter
 from tkinter import ttk
 
 
 class Dashboard(tkinter.Toplevel):
+    """
+    serves as blueprint for the dashboard class
+    """
+
     def __init__(self, parent):
+        """
+        serves as the constructore for a dashboard object
+        """
         super().__init__(parent)
-        photo = tkinter.PhotoImage(file = 'icon.png')
+        photo = tkinter.PhotoImage(file='icon.png')
         self.wm_iconphoto(False, photo)
         self.minsize(500, 300)
         self.title("Overlord's Dashboard")
         self.map_tree = self.make_tree("top", "Map", "Map ID")
         self.turn_tree = self.make_tree("bottom", "Tick", "Action")
-        #self.map_tree.insert('', 'end', text='Listbox', values=('15KB', 'Yesterday'))
-        
-    #https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
-    def make_tree(self, aligment, column1, column2):
-        # Using treeview widget
-        treev = ttk.Treeview(self, selectmode ='browse')
+        # self.map_tree.insert('', 'end', text='Listbox', values=('15KB', 'Yesterday'))
 
-                # Calling pack method w.r.to treeview
-        treev.pack(side = aligment)
-        
+    # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
+    def make_tree(self, aligment, column1, column2):
+        """
+        Builds trees for the dasboard to use, dashboards typically serve spreadhsheets in the gui.
+        """
+        # Using treeview widget
+        treev = ttk.Treeview(self, selectmode='browse')
+
+        # Calling pack method w.r.to treeview
+        treev.pack(side=aligment)
+
         # Defining number of columns
         treev["columns"] = ("1", "2")
-        
+
         # Defining heading
         treev['show'] = 'headings'
-        
+
         # Assigning the width and anchor to  the
         # respective columns
-        treev.column("1", width = 180, anchor ='c')
-        treev.column("2", width = 180, anchor ='se')
-        
+        treev.column("1", width=180, anchor='c')
+        treev.column("2", width=180, anchor='se')
+
         # Assigning the heading names to the
         # respective columns
-        treev.heading("1", text = column1)
-        treev.heading("2", text = column2)
+        treev.heading("1", text=column1)
+        treev.heading("2", text=column2)
         return treev
-    #https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
+    # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
--- map.py
+++ map.py
@@ -1,28 +1,35 @@
 #!/usr/local/bin/python
-
+"""
+Defines Map class along with the methods and attributs that it uses.
+"""
 import random
 import tkinter
 
 
 class Map(tkinter.Toplevel):
-    def __init__(self, parent):
+    """
+    Serves as a blueprint for a map object
+    """
+
+    def __init__(self, parent, title):
         super().__init__(parent)
         self.geometry("300x200+300+0")
-        self.title("Map")
-        self.log = tkinter.Text(self, width=30,  height=30)
+        self.title(title)
+        self.log = tkinter.Text(self, width=30, height=30)
         self.log.pack()
 
-
     def write_up(self, string, x, y):
+        """
+        This writes to the map in certain unicode characters
+        """
         unicode_dict = {
-            'wall' : u'\u2589',
-            'acid' : u'\u2600',
-            'minerals' : u'\u2662',
-            'drone' : u'\u26DF',
-            'deployment' : u'\u25BD',
-            'unknown' : u'\u26F6'
+            'wall': u'\u2589',
+            'acid': u'\u2600',
+            'minerals': u'\u2662',
+            'drone': u'\u26DF',
+            'deployment': u'\u25BD',
+            'unknown': u'\u26F6'
         }
         unicode_character = unicode_dict[string]
         coordinates = f'{y}.{x}'
         self.log.insert(coordinates, unicode_character)
-        

84d25367a3fbe89c5fd2b563e4d0c7794121aa66
Raymone Miller <miller_raymone@hotmail.com>
2023-03-03 11:45:45 -0500

    Failed test: test_miner_mine_mineral

--- test/test_miner.py
+++ test/test_miner.py
@@ -1,6 +1,8 @@
 """Test class for miner drone zerg units."""
 from test.base_drone_tester import BaseDroneTester
+from test.testing_utils import TestingUtils
 
+from utils import Icon, Tile
 from zerg.drones import MinerDrone
 
 
@@ -26,3 +28,12 @@ class TestMiner(BaseDroneTester):
         for drone_n in range(len(self.custom_drones_)):
             self.assertIsInstance(self.custom_drone_stats_[drone_n].init, type)
             self.assertIsInstance(self.custom_drones_[drone_n], MinerDrone)
+
+    def test_miner_mine_mineral(self):
+        for _ in range(self.RANDOM_TEST_RUNS):
+            mineral_tile = Tile(
+                TestingUtils._randomize_coordinate(), Icon.MINERAL
+            )
+            self._register_tile(mineral_tile)
+            self._travel(self.base_miner_, dest=mineral_tile)
+            self.assertEqual(mineral_tile.icon, Icon.EMPTY)

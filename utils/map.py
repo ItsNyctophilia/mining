5c01c3eafeb09ca8427d373b752ed54a08dce938
Raymone Miller <miller_raymone@hotmail.com>
2023-03-06 18:01:27 -0500

    Make Map subscriptable

--- utils/map.py
+++ utils/map.py
@@ -1,7 +1,7 @@
 """A map made up of tiles."""
 
 import heapq as heap
-from typing import Dict, List, Optional, Set, Tuple
+from typing import Dict, List, Optional, Set, Tuple, Union
 
 from .context import Context
 from .coordinate import Coordinate
@@ -22,10 +22,11 @@ class Map:
             context (Optional[Context], optional): The origin of the map.
                 Defaults to None.
         """
+        self.adjacency_list: Dict[Tile, List[Tile]] = {}
+        self._stored_tiles_: Dict[Coordinate, Tile] = {}
         if context:
             self.origin = Coordinate(context.x, context.y)
             self.update_context(context, True)
-        self.adjacency_list: Dict[Tile, List[Tile]] = {}
 
         # dict{Tile: [Tile, Tile, Tile, Tile]}
 
@@ -86,6 +87,63 @@ class Map:
             current_tile = Tile(current_coord, Icon(symbol))
             neighbors.append(current_tile)
             if current_tile not in self.adjacency_list:
+                # TODO: update this new tile neighbor to reference start tile
                 self.adjacency_list.update({current_tile: []})
+                self._stored_tiles_[current_coord] = current_tile
 
         self.adjacency_list.update({start_tile: neighbors})
+
+    def get(
+        self, key: Union[Tile, Coordinate], default: Optional[Tile] = None
+    ) -> Optional[Tile]:
+        """Get the tile with the specified coordinates from the map.
+
+        A Tile or Coordinate object may be passed in as the key; if a Tile is
+        given, only it's coordinate attribute will be used in look up.
+
+        Args:
+            key (Union[Tile, Coordinate]): The key to look up.
+            default (Optional[Tile], optional): A value to return if the
+                key is not found. Defaults to None.
+
+        Returns:
+            Optional[Tile]: The Tile within this map.
+        """
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return default
+
+    def __getitem__(self, key: Union[Tile, Coordinate]) -> Tile:
+        """Get the tile with the specified coordinates from the map.
+
+        A Tile or Coordinate object may be passed in as the key; if a Tile is
+        given, only it's coordinate attribute will be used in look up.
+
+        Args:
+            key (Union[Tile, Coordinate]): The key to look up.
+
+        Raises:
+            KeyError: If no Tile with the given coordinates exists in this map.
+
+        Returns:
+            Tile: The Tile within this map.
+        """
+        if isinstance(key, Tile):
+            key = key.coordinate
+        if not (tile := self._stored_tiles_.get(key, None)):
+            raise KeyError(repr(key))
+        return tile
+
+    def __iter__(self):
+        yield from self.adjacency_list
+
+    def __repr__(self) -> str:
+        """Return a representation of this object.
+
+        The string returned by this method is not valid for a call to eval.
+
+        Returns:
+            str: The string representation of this object.
+        """
+        return f"Map({list(self.adjacency_list)})"

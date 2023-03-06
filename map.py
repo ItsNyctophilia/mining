f766f180d8f0472431fab3191cd72a121639201a
Josh <jcarroll0310@gmail.com>
2023-03-06 14:19:53 -0500

    add translation function for tiles

--- map.py
+++ map.py
@@ -36,3 +36,29 @@ class Map(tkinter.Toplevel):
         unicode_character = unicode_dict[string]
         coordinates = f'{y}.{x}'
         self.log.insert(coordinates, unicode_character)
+
+    def prepare_map(self):    
+        """
+        This prepares a map in the beginning by filling it with unknown characters
+        """
+        for x in range(200):
+            for y in range(200):
+                self.log.insert(f'{x}.{y}', u'\u26F6')
+            self.log.insert(f'{x}.200', '\n')
+
+    def translate_tile(self, tile):
+        """
+        This writes to the map in certain unicode characters
+        """
+        unicode_dict = {
+            '#': u'\u2589',
+            '~': u'\u2600',
+            '*': u'\u2662',
+            'Z': u'\u26DF',
+            '_': u'\u25BD',
+            'unknown': u'\u26F6'
+        }
+        unicode_character = unicode_dict[tile.icon]
+        print(unicode_character)
+        coordinates = f'{tile.coordinate.x}.{tile.coordinate.y}'
+        self.log.insert(coordinates, unicode_character)

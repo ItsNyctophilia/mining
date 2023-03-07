2d8838fc5cd6be00c7bb5bd29f09c0e60cbd2874
Raymone Miller <miller_raymone@hotmail.com>
2023-03-06 21:45:19 -0500

    Overload Map.get() method for type hinting

--- utils/map.py
+++ utils/map.py
@@ -1,7 +1,7 @@
 """A map made up of tiles."""
 
 import heapq as heap
-from typing import Dict, List, Optional, Set, Tuple, Union
+from typing import Dict, List, Optional, Set, Tuple, Union, overload
 
 from .context import Context
 from .coordinate import Coordinate
@@ -93,9 +93,17 @@ class Map:
 
         self.adjacency_list.update({start_tile: neighbors})
 
+    @overload
     def get(
-        self, key: Union[Tile, Coordinate], default: Optional[Tile] = None
+        self, key: Union[Tile, Coordinate], default: None
     ) -> Optional[Tile]:
+        pass
+
+    @overload
+    def get(self, key: Union[Tile, Coordinate], default: Tile) -> Tile:
+        pass
+
+    def get(self, key, default):
         """Get the tile with the specified coordinates from the map.
 
         A Tile or Coordinate object may be passed in as the key; if a Tile is
